56675a5086ac8cdaa7660c0e313293381a5b59e7
Rewa17Shankar <shankarewajii@gmail.com>
2024-09-26 20:29:44 +0530

    cc

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
 document.getElementById("userForm").addEventListener("submit", function(event){
     event.preventDefault();
-    window.location.href = "http://127.0.0.1:5501/index.html";
+    window.location.href = "index.html";
 })
